31ee17fbdbce70212422e893e397d7a9a8d5a690
Siarhei Beliakou <sbeliakou@gmail.com>
2018-07-19 12:25:25 +0300

    sample vagrantfile

--- scripts/base.sh
+++ scripts/base.sh
@@ -2,8 +2,7 @@
 
 echo "debug: Executing scripts/base.sh"
 
-yum install -y deltarpm
-yum install -y epel-release
+yum install -y deltarpm epel-release
 
 yum -y update
 yum -y install gcc make gcc-c++ kernel-devel kernel-headers perl wget bzip2 tree
