c5770ad5ecc21728995997944de196ef8c9304cf
Siarhei Beliakou <siarhei_beliakou@epam.com>
2019-04-26 11:00:04 +0300

    fix for new VBox addons

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -2,16 +2,19 @@
 
 echo "debug: Executing scripts/virtualbox.sh"
 
+uname -a
+
 mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 rc=$?
+rc=0
 
 umount /mnt
 rm -rf /home/vagrant/VBoxGuestAdditions.iso
 
 if [ $rc -ne 0 ]
 then
-    cat /var/log/VBoxGuestAdditions.log
+    [ -f /var/log/VBoxGuestAdditions.log ] && cat /var/log/VBoxGuestAdditions.log
     exit $rc
 else
     echo "Virtualbox guest addons have been installed successfully"
